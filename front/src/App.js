e6d40a108583061cce83384149cfbab9d251fe9d
indgomez <ext_indgomez@mercadolibre.com.co>
2021-12-20 08:53:23 -0500

    changes frontend

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css'
 import {ServicePolling} from './services/servicePolling'
+import React, {Component, useState} from 'react';
+import {Render} from 'react-dom';
 
 export const App = () => {
   let socket = null;
+  let arrChat = {};
   const connectSocket = () => {
     socket = ServicePolling();
     socket.on("data", (data) => {
@@ -13,23 +16,28 @@ export const App = () => {
   const disconnectSocket = () => {
     //socket.disconnect();
   }
+  const [chat, setChat] = useState(0);
+
+  const getValueInput = (value) => {
+    arrChat =  [{...arrChat}, value];
+    setChat(arrChat)
+  }
   return (
     <div className="App">
       <header className="App-header">
+        <div className="containerChat">
+          <div className="showChat">
+          {chat}
+          </div>
+          <div className="inputWrite">
+            <input type="text" className="inputText" onChange={ (e) => setChat(e.target.value) }>
+            </input>
+            <button className="button"  >Enviar</button>
+          </div>
+        </div>
         {connectSocket()}
         <button onClick={disconnectSocket()}>desconectar</button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
